702d213905e94a0210b26d0879021e5beb295690
HwangHyeryeong <hrh4225@gmail.com>
2023-11-10 16:07:36 +0900

    chore: ⚙️ url 패턴 수정

--- goods/urls.py
+++ goods/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<category>/', views.selectGoodsList, name="selectGoodsList"),
-    path('detail/<goodsCode>/', views.selectGoodsInfo, name="selectGoodsInfo"),
+    path('<category>', views.selectGoodsList, name="selectGoodsList"),
+    path('detail/<goodsCode>', views.selectGoodsInfo, name="selectGoodsInfo"),
 ]
